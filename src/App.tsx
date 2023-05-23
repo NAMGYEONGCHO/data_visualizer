0cb79785f95581d9faa7b103245d93f727f3c97f
NAMGYEONGCHO <official2002mk@gmail.com>
2023-05-23 18:03:21 +0200

    added test code for night mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import useNightMode from "./hooks/useNightMode";
+
 function App() {
+  const [nightMode, setNightMode] = useNightMode();
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="h-screen w-screen p-6 bg-white dark:bg-black">
+      <button
+        className="dark:text-white text-black bg-sky-500 dark:bg-green-500"
+        onClick={() => setNightMode(nightMode === "light" ? "dark" : "light")}
+      >
+        TOGGLE THEME
+      </button>
+    </div>
   );
 }
 
