0cdd25421568138c47f21cfade6b8397add6f788
NAMGYEONGCHO <official2002mk@gmail.com>
2023-05-31 12:05:08 +0200

    added semantic HTML

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -24,61 +24,61 @@ const Dashboard = () => {
   const chartHeight300 = 300;
   
   return (
-    <>
-    <div className="flex flex-grow flex-col">
-      {/* Top Row */}
-      <div className={`flex flex-wrap justify-between`}>
-        <div className={`w-full sm:w-1/2 md:w-1/4 rounded-md relative p-1`}>
-          <div style={{height: chartHeight200}}>
-            <LineChart/>
+    <main>
+      <section className="flex flex-grow flex-col">
+        {/* Top Row */}
+        <article className={`flex flex-wrap justify-between`}>
+          <div className={`w-full sm:w-1/2 md:w-1/4 rounded-md relative p-1`}>
+            <div style={{height: chartHeight200}}>
+              <LineChart/>
+            </div>
           </div>
-        </div>
-        <div className={`w-full sm:w-1/2 md:w-1/4 rounded-md relative p-1`}>
-          <div style={{height: chartHeight200}}>
-            <BarChart/>
+          <div className={`w-full sm:w-1/2 md:w-1/4 rounded-md relative p-1`}>
+            <div style={{height: chartHeight200}}>
+              <BarChart/>
+            </div>
           </div>
-        </div>
-        <div className={`w-full sm:w-1/2 md:w-1/4 rounded-md relative p-1`}>
-          <div style={{height: chartHeight200}}>
-            <PieChart data={pieData} />
+          <div className={`w-full sm:w-1/2 md:w-1/4 rounded-md relative p-1`}>
+            <div style={{height: chartHeight200}}>
+              <PieChart data={pieData} />
+            </div>
           </div>
-        </div>
-        <div className={`w-full sm:w-1/2 md:w-1/4 rounded-md relative p-1`}>
-          <div style={{height: chartHeight200}}>
-            <PieChartLegent data={pieData} />
+          <div className={`w-full sm:w-1/2 md:w-1/4 rounded-md relative p-1`}>
+            <div style={{height: chartHeight200}}>
+              <PieChartLegent data={pieData} />
+            </div>
           </div>
-        </div>
-      </div>
+        </article>
 
-      {/* Center Row */}
-      <div className={`flex flex-wrap`}>
-        <div className="w-full sm:w-2/3 md:w-3/4 relative p-1">
-          <div style={{height: chartHeight300}}>
-            <LineChart />
+        {/* Center Row */}
+        <article className={`flex flex-wrap`}>
+          <div className="w-full sm:w-2/3 md:w-3/4 relative p-1">
+            <div style={{height: chartHeight300}}>
+              <LineChart />
+            </div>
           </div>
-        </div>
-        <div className={`w-full sm:w-1/3 md:w-1/4 rounded-md relative p-1`}>
-          <div style={{height: chartHeight300}}>
-          <ScatterPlotChart data={scatterPlotData}/>
+          <div className={`w-full sm:w-1/3 md:w-1/4 rounded-md relative p-1`}>
+            <div style={{height: chartHeight300}}>
+            <ScatterPlotChart data={scatterPlotData}/>
+            </div>
           </div>
-        </div>
-      </div>
+        </article>
 
-      {/* Bottom Row */}
-      <div className={`flex flex-wrap justify-between`} style={{ height: chartHeight200 }}>
-        <div className="bg-green-500 rounded-md w-full sm:w-1/2 md:w-1/3">Column 1</div>
-        <div className="bg-blue-500 rounded-md w-full sm:w-1/2 md:w-1/3">Column 2</div>
-        <div className="bg-cyan-500 rounded-md w-full sm:w-1/2 md:w-1/3">Column 3</div>
-      </div>
-     <Box height={4}/>
-    </div>
-    <div className='grid grid-cols-4 gap-2' style={{ height: chartHeight200 }}>
-      <div className="bg-red-500 rounded-md shadow-xl min-h-[200] w-full">Column 1</div>
-      <div className="bg-orange-500 rounded-md shadow-xl min-h-[200] w-full">Column 2</div>
-      <div className="bg-yellow-500 rounded-md shadow-xl min-h-[200] w-full">Column 3</div>
-      <div className="bg-purple-500 rounded-md shadow-xl min-h-[200] w-full">Column 3</div>
-    </div>
-    </>
+        {/* Bottom Row */}
+        <article className={`flex flex-wrap justify-between`} style={{ height: chartHeight200 }}>
+          <div className="bg-green-500 rounded-md w-full sm:w-1/2 md:w-1/3">Column 1</div>
+          <div className="bg-blue-500 rounded-md w-full sm:w-1/2 md:w-1/3">Column 2</div>
+          <div className="bg-cyan-500 rounded-md w-full sm:w-1/2 md:w-1/3">Column 3</div>
+        </article>
+      </section>
+      <Box height={4}/>
+      <section className='grid grid-cols-4 gap-2' style={{ height: chartHeight200 }}>
+        <div className="bg-red-500 rounded-md shadow-xl min-h-[200] w-full">Column 1</div>
+        <div className="bg-orange-500 rounded-md shadow-xl min-h-[200] w-full">Column 2</div>
+        <div className="bg-yellow-500 rounded-md shadow-xl min-h-[200] w-full">Column 3</div>
+        <div className="bg-purple-500 rounded-md shadow-xl min-h-[200] w-full">Column 3</div>
+      </section>
+    </main>
   );
 };
 
